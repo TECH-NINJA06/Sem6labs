5ae3081a08207797d867872aebbd13b1664bcde7
Harsh Telang <telangharsh06@gmail.com>
2026-07-16 19:03:11 +0530

    ...

--- MCN py/error detection/CRC.py
+++ MCN py/error detection/CRC.py
@@ -1,32 +1,13 @@
-#include <iostream>
-#include <string>
+data = input("Enter Data bits: ")
+gen = input("Enter Generator bits: ")
 
-using namespace std;
+div = list(data + '0' * (len(gen) - 1))
 
-int main() {
-    string data, gen;
-    cout << "Enter Data bits: "; 
-    cin >> data;
-    cout << "Enter Generator bits: "; 
-    cin >> gen;
+for i in range(len(data)):
+    if div[i] == '1':
+        for j in range(len(gen)):
+            div[i+j] = str(int(div[i+j]) ^ int(gen[j]))
 
-    string div = data;
-    for(int i = 0; i < gen.length() - 1; i++) {
-        div += '0';
-    }
-
-    for(int i = 0; i <= div.length() - gen.length(); ) {
-        for(int j = 0; j < gen.length(); j++) {
-            div[i+j] = (div[i+j] == gen[j]) ? '0' : '1';
-        }
-        while(i < div.length() && div[i] != '1') {
-            i++;
-        }
-    }
-
-    string crc = div.substr(data.length());
-    cout << "Generated CRC: " << crc << endl;
-    cout << "Transmitted Data: " << data + crc << endl;
-    
-    return 0;
-}
+crc = "".join(div)[-(len(gen)-1):]
+print(f"Generated CRC: {crc}")
+print(f"Transmitted Data: {data + crc}")
